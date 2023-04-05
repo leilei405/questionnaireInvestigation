cb3af3f2bd123590efea3e01dd3c1e3fd4a4d6dc
fll <fllning@163.com>
2023-04-05 14:03:26 +0800

    fix: 删除未使用声明

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -3,9 +3,6 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const app = () => {
-    console.log(1);
-  };
   return (
     <div className="App">
       <header className="App-header">
