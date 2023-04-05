df9d53b366e960dcc44ea5e5551a8ebc16a8df34
fll <fllning@163.com>
2023-04-05 14:01:24 +0800

    feat: eslint 规则配置

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const app = () => {
+    console.log(1);
+  };
   return (
     <div className="App">
       <header className="App-header">
