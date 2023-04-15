84d0f82f20947192bb79ac7e10d426368fdbbd4f
fll <fllning@163.com>
2023-04-15 15:59:35 +0800

    feat: useCallback

--- react-demo/src/components/useCallbackDemo.tsx
+++ react-demo/src/components/useCallbackDemo.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { FC, useState, useCallback } from 'react'
 
-function UseCallbackDemo() {
+export const UseCallbackDemo: FC = () => {
+  const [text, setText] = useState('useCallback')
+  const fn1 = () => console.log('fn1 useCallback', text)
+  // useCallback 就是 useMemo 的语法糖，和 useMemo 一样
+  const fn2 = useCallback(() => {
+    console.log('fn2 useCallback', text)
+  }, [text])
   return (
     <div>
-      UseCallbackDemo
+      <h1>UseCallbackDemo</h1>
+      <button onClick={fn1}>FN1</button>
+      <button onClick={fn2}>FN2</button>
+      <input value={text} onChange={e => setText(e.target.value)} />
     </div>
-  );
+  )
 }
-
-export default UseCallbackDemo;
