0f97a8c29f0fbe400431fcdea63996fa081d4cdd
fll <fllning@163.com>
2023-04-16 21:10:28 +0800

    feat: 完成404页面

--- question-demo/src/pages/NotFound.tsx
+++ question-demo/src/pages/NotFound.tsx
@@ -1,10 +1,20 @@
 import React, { FC } from 'react'
-
+import { useNavigate } from 'react-router-dom'
+import { Button, Result } from 'antd'
+import { MANAGE_INDEX_PATHNAME } from '../router'
 const NotFound: FC = () => {
+  const nav = useNavigate()
   return (
-    <div>
-      <h1>NotFound</h1>
-    </div>
+    <Result
+      status="404"
+      title="404"
+      subTitle="很抱歉,你访问的页面不存在,输入正确地址"
+      extra={
+        <Button type="primary" onClick={() => nav(MANAGE_INDEX_PATHNAME)}>
+          返回首页
+        </Button>
+      }
+    />
   )
 }
 export default NotFound
