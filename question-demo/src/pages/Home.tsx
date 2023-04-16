ec5cca35e973e42aac7894bcd49d041bb615f4dc
fll <fllning@163.com>
2023-04-16 15:52:35 +0800

    feat: Home 页面美化

--- question-demo/src/pages/Home.tsx
+++ question-demo/src/pages/Home.tsx
@@ -1,19 +1,21 @@
 import React, { FC } from 'react'
+import styles from './Home.module.scss'
+import { Button, Typography } from 'antd'
 import { useNavigate, Link } from 'react-router-dom'
-import { Button } from 'antd'
+import { MANAGE_INDEX_PATHNAME } from '../router'
+const { Title, Paragraph } = Typography
 const Home: FC = () => {
   const nav = useNavigate()
-  const clickHandler = () => {
-    nav('/login')
-  }
   return (
-    <div>
-      <h1>Home</h1>
-      <div>
-        <Button type="primary" onClick={clickHandler}>
-          登录跳转Login
-        </Button>
-        <Link to="/register">链接跳转注册页</Link>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <Title>问卷调查 | 在线投票</Title>
+        <Paragraph>已累计创建 1000份, 发布问卷1000份, 收到答卷10000</Paragraph>
+        <div>
+          <Button type="primary" onClick={() => nav(MANAGE_INDEX_PATHNAME)}>
+            开始使用
+          </Button>
+        </div>
       </div>
     </div>
   )
