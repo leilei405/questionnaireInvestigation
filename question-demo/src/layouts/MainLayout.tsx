fad12e5f540a01c77200b41103f698de6065a261
fll <fllning@163.com>
2023-04-16 15:03:26 +0800

    feat: antd 重构mainLayout与manageLayout

--- question-demo/src/layouts/MainLayout.tsx
+++ question-demo/src/layouts/MainLayout.tsx
@@ -1,15 +1,22 @@
 import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
+import { Layout } from 'antd'
+const { Header, Footer, Content } = Layout
+import styles from './MainLayout.module.scss'
 const MainLayout: FC = () => {
   return (
-    <div>
-      <div>Main Header</div>
-      <div>
-        {/* Outlet 类似于vue的slot */}
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>logo</div>
+        <div className={styles.right}>登录</div>
+      </Header>
+      <Content className={styles.main}>
         <Outlet />
-      </div>
-      <div>Main Footer</div>
-    </div>
+      </Content>
+      <Footer className={styles.footer}>
+        Amorous 问卷调查 在线问卷 &copy;2023 - author Amorous
+      </Footer>
+    </Layout>
   )
 }
 export default MainLayout
--- question-demo/src/layouts/ManageLayout.tsx
+++ question-demo/src/layouts/ManageLayout.tsx
@@ -1,18 +1,44 @@
 import React, { FC } from 'react'
-import { Outlet } from 'react-router-dom'
-import styles from './ManageLayout1.module.scss'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
+import { Button, Space, Divider } from 'antd'
+import { BarsOutlined, PlusOutlined, StarOutlined, DeleteOutlined } from '@ant-design/icons'
+import styles from './ManageLayout.module.scss'
 const ManageLayout: FC = () => {
+  const nav = useNavigate()
+  const { pathname } = useLocation()
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        <p>ManageLayout Left</p>
-        <button>创建问卷</button>
-        <br />
-        <a href="#">我的问卷</a>
-        <br />
-        <a href="#">星标问卷</a>
-        <br />
-        <a href="#">回收站</a>
+        <Space direction="vertical">
+          <Button type="primary" size="large" icon={<PlusOutlined />}>
+            新建问卷
+          </Button>
+          <Divider />
+          <Button
+            type={pathname.startsWith('/manage/list') ? 'primary' : 'text'}
+            size="large"
+            icon={<BarsOutlined />}
+            onClick={() => nav('/manage/list')}
+          >
+            我的问卷
+          </Button>
+          <Button
+            type={pathname.startsWith('/manage/star') ? 'primary' : 'text'}
+            size="large"
+            icon={<StarOutlined />}
+            onClick={() => nav('/manage/star')}
+          >
+            星标问卷
+          </Button>
+          <Button
+            type={pathname.startsWith('/manage/trash') ? 'primary' : 'text'}
+            size="large"
+            icon={<DeleteOutlined />}
+            onClick={() => nav('/manage/trash')}
+          >
+            回收站
+          </Button>
+        </Space>
       </div>
       <div className={styles.right}>
         <Outlet />
