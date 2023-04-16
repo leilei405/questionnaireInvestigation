61a81c6391c0cab2f40889c7d3b651eb80bc9ed9
fll <fllning@163.com>
2023-04-16 15:19:50 +0800

    feat:增加logo

--- question-demo/src/layouts/MainLayout.tsx
+++ question-demo/src/layouts/MainLayout.tsx
@@ -3,11 +3,14 @@ import { Outlet } from 'react-router-dom'
 import { Layout } from 'antd'
 const { Header, Footer, Content } = Layout
 import styles from './MainLayout.module.scss'
+import Logo from '../components/Logo'
 const MainLayout: FC = () => {
   return (
     <Layout>
       <Header className={styles.header}>
-        <div className={styles.left}>logo</div>
+        <div className={styles.left}>
+          <Logo />
+        </div>
         <div className={styles.right}>登录</div>
       </Header>
       <Content className={styles.main}>
