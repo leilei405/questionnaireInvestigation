bc4364340a294e85a3862bc7779e7f1df72d2b38
fll <fllning@163.com>
2023-05-26 00:51:09 +0800

    feat: 定义画布中的俩个组件Input与title

---
+++ question-demo/src/components/questionComponents/questionInput/Components.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react'
+import { Input, Typography } from 'antd'
+import { QuestionInputDefaultProps, QuestionInputPropsType } from './inputType'
+
+const { Paragraph } = Typography
+
+export const QuestionInput: FC<QuestionInputPropsType> = (props: QuestionInputPropsType) => {
+  const { title, placeholder } = { ...QuestionInputDefaultProps, ...props }
+
+  return (
+    <div>
+      <Paragraph strong>{title}</Paragraph>
+      <div>
+        <Input placeholder={placeholder} />
+      </div>
+    </div>
+  )
+}
---
+++ question-demo/src/components/questionComponents/questionTitle/Components.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react'
+import { Typography } from 'antd'
+import { QuestionTitleDefaultProps, QuestionTitlePropsType } from './titleType'
+
+const { Title } = Typography
+
+export const QuestionTitle: FC<QuestionTitlePropsType> = (props: QuestionTitlePropsType) => {
+  const { text = '', level = 1, isCenter = false } = { ...QuestionTitleDefaultProps, ...props }
+
+  const genFontSize = (level: number) => {
+    if (level === 1) return '24px'
+    if (level === 2) return '20px'
+    if (level === 3) return '16px'
+    return '16px'
+  }
+  return (
+    <Title
+      level={level}
+      style={{
+        textAlign: isCenter ? 'center' : 'start',
+        marginBottom: 0,
+        fontSize: genFontSize(level),
+      }}
+    >
+      {text}
+    </Title>
+  )
+}
