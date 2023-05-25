75d460d4761c2e701782f63e0b292efbf67ddd21
fll <fllning@163.com>
2023-05-25 21:28:31 +0800

    feat:开发UI组件拆分,实现Y轴滚动

--- question-demo/src/pages/question/Edit/index.tsx
+++ question-demo/src/pages/question/Edit/index.tsx
@@ -1,13 +1,25 @@
 import React, { FC } from 'react'
 import { useLoadQuestionData } from '../../../hooks/useLoadQuestionData'
-
+import styles from './index.module.scss'
 const Edit: FC = () => {
   // 获取问卷详情
   const { loading, data } = useLoadQuestionData()
   return (
-    <div>
-      <h1>Edit</h1>
-      <div>{loading ? '123' : JSON.stringify(data)}</div>
+    <div className={styles.container}>
+      {/* 顶部 */}
+      <div className={styles.header}>Header</div>
+      {/* 主体内容区域 */}
+      <div className={styles['content-wrapper']}>
+        <div className={styles.content}>
+          <div className={styles.left}>Left</div>
+          <div className={styles.main}>
+            <div className={styles['canvas-wrapper']}>
+              <div style={{ height: '900px' }}>画布滚动</div>
+            </div>
+          </div>
+          <div className={styles.right}>Right</div>
+        </div>
+      </div>
     </div>
   )
 }
