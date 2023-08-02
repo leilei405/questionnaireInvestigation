205de1da0a9df6c1c7f7041e9150c997316424cd
fll <fllning@163.com>
2023-08-02 17:41:02 +0800

    faet: 为段落标签添加换行功能

--- question-demo/src/components/questionComponents/QuestionParagraph/ComponentParagraph.tsx
+++ question-demo/src/components/questionComponents/QuestionParagraph/ComponentParagraph.tsx
@@ -6,9 +6,21 @@ const { Paragraph } = Typography
 
 const ComponentParagraph: FC<QuestionParagraphPropsType> = (props: QuestionParagraphPropsType) => {
   const { text = '', isCenter = false } = { ...QuestionParagraphDefaultProps, ...props }
+  // const n = text.replaceAll('\n', '<br>')
+  const textList = text.split('\n')
   return (
     <Paragraph style={{ textAlign: isCenter ? 'center' : 'start', marginBottom: 0 }}>
-      {text}
+      {/* 1.不建议使用dangerouslySetInnerHTML 方式 */}
+      {/* <span dangerouslySetInnerHTML={{ __html: n }}></span> */}
+      {/* 2.map */}
+      {textList.map((item, index) => {
+        return (
+          <span key={index}>
+            {index > 0 && <br />}
+            {item}
+          </span>
+        )
+      })}
     </Paragraph>
   )
 }
