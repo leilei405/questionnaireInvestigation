ea26e1a0e45ed49376f3165dfd6c4a47a1610a2c
fll <fllning@163.com>
2023-12-03 00:33:17 +0800

    feat: 保存问卷信息-判断loading防止连续触发

--- question-demo/src/pages/question/Edit/EditHeader.tsx
+++ question-demo/src/pages/question/Edit/EditHeader.tsx
@@ -1,13 +1,17 @@
 import React, { ChangeEvent, FC, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { Button, Space, Typography, Input } from 'antd'
-import { LeftOutlined, CheckOutlined, EditOutlined } from '@ant-design/icons'
+import { LeftOutlined, CheckOutlined, EditOutlined, LoadingOutlined } from '@ant-design/icons'
+import { useRequest, useKeyPress } from 'ahooks'
 import useGetPageInfo from '../../../hooks/useGetPageInfo'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
 import styles from './EditHeader.module.scss'
 import EditToolbar from './EditToolbar' // 中间工具栏
 import { useDispatch } from 'react-redux'
 import { changePageTitle } from '../../../store/pageInfoReducer'
+import { updateQuestionServices } from '../../../services/question'
 
+// 标题组件
 const TitleElem: FC = () => {
   const [editState, setEditState] = useState(false)
   const { title } = useGetPageInfo()
@@ -44,6 +48,39 @@ const TitleElem: FC = () => {
   )
 }
 
+// 保存按钮组件
+const SaveButton: FC = () => {
+  const { id } = useParams()
+  const { componentList } = useGetComponentInfo() // 获取组件列表信息
+  const pageInfo = useGetPageInfo() // 获取页面信息
+
+  // 保存问卷信息
+  const { loading, run: save } = useRequest(
+    async () => {
+      if (!id) return
+      await updateQuestionServices(id, { ...pageInfo, componentList })
+    },
+    {
+      manual: true,
+    }
+  )
+
+  // 快捷键 ctrl + s  Or command + s  保存问卷信息
+  useKeyPress(['ctrl.s', 'meta.s'], (event: KeyboardEvent) => {
+    event.preventDefault() // 阻止默认行为
+    if (!loading) save()
+  })
+  return (
+    <Button
+      onClick={save}
+      disabled={loading}
+      icon={loading ? <LoadingOutlined /> : <CheckOutlined />}
+    >
+      保存
+    </Button>
+  )
+}
+
 const { Title } = Typography
 const EditHeader: FC = () => {
   const nav = useNavigate()
@@ -63,7 +100,7 @@ const EditHeader: FC = () => {
         </div>
         <div className={styles.right}>
           <Space>
-            <Button icon={<CheckOutlined />}>保存</Button>
+            <SaveButton />
             <Button type="primary">发布</Button>
           </Space>
         </div>
