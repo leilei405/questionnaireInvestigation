f78639cca110483f3c11af4dc3131a0cbc0396ba
fll <fllning@163.com>
2023-12-04 23:35:47 +0800

    feat: 组件列表拖拽排序,抽离公共组件

---
+++ question-demo/src/components/DragSortable/SortableContainer.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from 'react'
+import {
+  DndContext,
+  closestCenter,
+  useSensor,
+  useSensors,
+  MouseSensor,
+  DragEndEvent,
+} from '@dnd-kit/core'
+import {
+  //   arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
+
+type PropsType = {
+  children: JSX.Element | JSX.Element[]
+  items: Array<{ id: string; [key: string]: any }>
+  onDragEnd: (oldIndex: number, newIndex: number) => void
+}
+
+const SortContainer: FC<PropsType> = (props: PropsType) => {
+  const { children, items, onDragEnd } = props
+
+  const sensors = useSensors(
+    useSensor(MouseSensor, {
+      activationConstraint: {
+        distance: 8,
+      },
+    })
+  )
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event
+    if (over == null) return
+    if (active.id !== over.id) {
+      const oldIndex = items.findIndex(c => c.fe_id === active.id)
+      const newIndex = items.findIndex(c => c.fe_id === over.id)
+      console.log(oldIndex, newIndex, '=======')
+      onDragEnd(oldIndex, newIndex)
+    }
+  }
+
+  return (
+    <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <SortableContext items={items} strategy={verticalListSortingStrategy}>
+        {children}
+      </SortableContext>
+    </DndContext>
+  )
+}
+
+export default SortContainer
