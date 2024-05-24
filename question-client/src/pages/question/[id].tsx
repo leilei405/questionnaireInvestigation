1687ed331cc689715dd0f2cc8a5ba8b8cfa77946
leilei405 <1601178425@qq.com>
2024-05-24 23:08:25 +0800

    feat: 添加隐藏域 样式修改

--- question-client/src/pages/question/[id].tsx
+++ question-client/src/pages/question/[id].tsx
@@ -1,28 +1,41 @@
 import React from "react";
 import QuestionInput from "@/components/questionComponents/questionInput";
 import QuestionRadio from "@/components/questionComponents/questionRadio";
+import styles from "./index.module.scss";
 import { PropsType } from "./types";
 export default function Question(props: PropsType) {
+  const handleSubmit = (values: any) => {
+    console.log(values);
+  };
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>问卷调查表单</h1>
-      <form>
-        <QuestionInput
-          fe_id="c1"
-          props={{ title: "职业", placeholder: "请输入职业名称" }}
-        />
-        <QuestionRadio
-          fe_id="c2"
-          props={{
-            title: "性别",
-            options: [
-              { text: "男", value: "1" },
-              { text: "女", value: "2" },
-            ],
-            isVertical: false,
-            value: "1",
-          }}
-        />
+      <form onSubmit={handleSubmit}>
+        <input type="hidden" name="questionId" value={props.id} />
+        <div className={styles.componentWrapper}>
+          <QuestionInput
+            fe_id="c1"
+            props={{ title: "职业", placeholder: "请输入职业名称" }}
+          />
+        </div>
+        <div className={styles.componentWrapper}>
+          <QuestionRadio
+            fe_id="c2"
+            props={{
+              title: "性别",
+              options: [
+                { text: "男", value: "1" },
+                { text: "女", value: "2" },
+              ],
+              isVertical: false,
+              value: "1",
+            }}
+          />
+        </div>
+
+        <div className={styles.submitBtnContainer}>
+          <button type="submit">提交</button>
+        </div>
       </form>
     </div>
   );
