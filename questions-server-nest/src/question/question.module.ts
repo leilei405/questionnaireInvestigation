ee6a690f505f88820edd83f6340141d3eafb8ae9
leilei405 <1601178425@qq.com>
2024-06-07 00:16:45 +0800

    feat: 创建schema 数据模型&&同步到数据库

--- questions-server-nest/src/question/question.module.ts
+++ questions-server-nest/src/question/question.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { QuestionController } from './question.controller';
 import { QuestionService } from './question.service';
+import { Question, QuestionSchema } from './question.schema'
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Question.name, schema: QuestionSchema }])],
   controllers: [QuestionController],
   providers: [QuestionService]
 })
