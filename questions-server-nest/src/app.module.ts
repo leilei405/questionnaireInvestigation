d777765289463f25e893b2971abf889b05293b15
leilei405 <1601178425@qq.com>
2024-06-06 23:52:30 +0800

    feat: nest 连接数据库，并且抽离配置信息

--- questions-server-nest/src/app.module.ts
+++ questions-server-nest/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [QuestionModule],
+  imports: [
+    ConfigModule.forRoot(), // 数据库全局配置信息
+    MongooseModule.forRoot(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`), // 不要写localhost
+    QuestionModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
