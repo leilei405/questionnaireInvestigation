fdc32181f706474f9ea22c2c5970177144a55d64
leilei405 <1601178425@qq.com>
2024-06-06 21:35:29 +0800

    feat: 获取url参数 query body  param

---
+++ questions-server-nest/src/question/dto/question.dto.ts
@@ -0,0 +1,8 @@
+export class QuestionDto {
+    // id: number;
+    // question: string;
+    // answer: string;
+    // isAnswered: boolean;
+    readonly title: string;
+    readonly desc: string;
+}
--- questions-server-nest/src/question/question.controller.ts
+++ questions-server-nest/src/question/question.controller.ts
@@ -1,14 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Param, Patch, Query, Body } from '@nestjs/common';
+import { QuestionDto } from './dto/question.dto';
 @Controller('question')
 export class QuestionController {
+    
+    // 获取问卷列表数据
     @Get()
-    findAll() {
-        return 'this is question list page';
+    findAll(
+        @Query('keyword') keyword: string,
+        @Query('page') page: number,
+        @Query('pageSize') pageSize: number,
+    ) {
+        return {
+            keyword,
+            page,
+            pageSize,
+        }
     }
 
+    // 获取单个问卷详情数据
+    @Get(':id')
+    findOne(
+        @Param('id') id: string,
+    ) {
+        console.log(id, '单个问卷详情');
+        
+        return { id }
+    }
+
+    // 问卷更新
+    @Patch(':id')
+    updateOne(
+        @Param('id') id: string,
+        @Body() questionDto: QuestionDto
+    ) {
+        console.log(id, questionDto, '更新问卷');
+        
+        return {
+            id,
+            ...questionDto,
+        };
+    }
+
+
+    // 问卷详情页
     @Get('detail')
     detail() {
-        return 'this is question detail page';
+        return '问卷详情页';
     }
 }
