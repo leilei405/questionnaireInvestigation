1f2411fa96ddc5c8f6657dfc7947bb3d36a19315
leilei405 <1601178425@qq.com>
2024-06-07 11:31:32 +0800

    feat: 实现Mongodb分页及搜索

--- questions-server-nest/src/question/question.controller.ts
+++ questions-server-nest/src/question/question.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param, Patch, Query, Body, HttpException, HttpStatus, Post } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Query, Body, Post, Delete } from '@nestjs/common';
 import { QuestionDto } from './dto/question.dto';
 import { QuestionService } from './question.service'
+import { count } from 'console';
 
 @Controller('question')
 export class QuestionController {
@@ -11,30 +12,37 @@ export class QuestionController {
     // 创建问卷
     @Post() 
     create(){
-        return this.questionService.create();
+        return this.questionService.questionCreate();
     }
     
     // 获取问卷列表
     @Get()
-    findAll(@Query('keyword') keyword: string,  @Query('page') page: number, @Query('pageSize') pageSize: number) {
-        return { keyword, page, pageSize }
+    async findAll(@Query('keyword') keyword: string,  @Query('page') page: number, @Query('pageSize') pageSize: number) {
+        const data = await this.questionService.questionList({ keyword, page, pageSize });
+        const total = await this.questionService.questionAllList({ keyword });
+        return {
+            data,
+            total,
+        }
     }
 
     // 获取单个问卷
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return this.questionService.findOne(id);        
+        return this.questionService.questionFindOne(id);        
+    }
+
+    // 删除问卷
+    @Delete(':id')
+    deleteOne(@Param('id') id: string) {
+        return this.questionService.questionDelete(id);
     }
 
     // 更新问卷
     @Patch(':id')
-    updateOne(@Param('id') id: string, @Body() questionDto: QuestionDto) {
-        return { id, ...questionDto };
+    updateOne(@Param('id') id: string, @Body() questionData: QuestionDto) {
+        return this.questionService.questionUpdate(id, questionData);
     }
 
-     // 添加过滤器之后 模拟错误信息
-    // @Get('textError')
-    // Test() {
-    //     throw new HttpException('获取数据失败', HttpStatus.BAD_REQUEST)
-    // }
+    
 }
--- questions-server-nest/src/question/question.service.ts
+++ questions-server-nest/src/question/question.service.ts
@@ -9,8 +9,33 @@ export class QuestionService {
         @InjectModel(Question.name) private readonly questionModel,
     ) {}
 
+    // 问卷列表查询
+    async questionList({keyword = '', page = 1, pageSize = 10}) {
+        const whereOpt: any = {};
+        if (keyword) {
+            const reg = new RegExp(keyword, 'i');
+            whereOpt.title = { $regex: reg }; // 模糊搜索
+        }
+        return await this.questionModel
+        .find(whereOpt)
+        .sort({ _id: -1 }) // 逆序排序
+        .skip((page - 1) * pageSize) // 分页
+        .limit(pageSize);
+    }
+
+    // 问卷列表查询总数
+    async questionAllList({ keyword = '' }) {
+        const whereOpt: any = {};
+        if (keyword) {
+            const reg = new RegExp(keyword, 'i');
+            whereOpt.title = { $regex: reg }; // 模糊搜索
+        }
+
+        return await this.questionModel.countDocuments(whereOpt);
+    }
+
     // 创建问卷
-    async create() {
+    async questionCreate() {
         const newQuestion = new this.questionModel({
             title: "questionTitle" + Date.now(),
             desc: "desc",
@@ -19,8 +44,17 @@ export class QuestionService {
     }
 
     // ID 查询单个问卷
-    async findOne(id: string) {
+    async questionFindOne(id: string) {
         return await this.questionModel.findById(id)
     }
 
+    // 更新问卷
+    async questionUpdate(id: string, questionData) {
+        return await this.questionModel.updateOne({ _id: id}, questionData)
+    }
+   
+    // 删除问卷
+    async questionDelete(id: string) {
+        return this.questionModel.findByIdAndDelete(id);
+    }
 }
